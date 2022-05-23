f4b00e55937cb219aa9632f000f42155f84aa8a4
JuluanQ <48562224+JuluanQ@users.noreply.github.com>
2022-05-23 19:43:15 +0200

    font weight

--- web/src/components/LeftMenu.js
+++ web/src/components/LeftMenu.js
@@ -2,27 +2,20 @@ import React from 'react';
 
 import '../assets/css/LeftMenu.css'
 
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const LeftMenu = () => {
-
-    const navigate = useNavigate()
-
     return (
         <div className='LeftMenuContainer'>
             <div className='LogoContainer'>
                 <img src='..\assets\icons\Logo_Blanc.png' alt='Logo Université Nantes' className='LogoUnivNanteMenu' />
             </div>
-
-            {navigate ?
-                <div className='MenusContainer'>
-                    <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><h3>Home</h3></NavLink>
-                    <NavLink to="/Demandes" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><h3>Demandes</h3></NavLink>
-                    <NavLink to="/Appareils" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><h3>Appareils</h3></NavLink>
-                    <h3 className='Deconnexion'>Se Déconnecter</h3>
-                </div>
-                : <></>
-            }
+            <div className='MenusContainer'>
+                <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><h3>Home</h3></NavLink>
+                <NavLink to="/Demandes" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><h3>Demandes</h3></NavLink>
+                <NavLink to="/Appareils" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><h3>Appareils</h3></NavLink>
+                <h3 className='Deconnexion'>Se Déconnecter</h3>
+            </div>
 
 
         </div>
