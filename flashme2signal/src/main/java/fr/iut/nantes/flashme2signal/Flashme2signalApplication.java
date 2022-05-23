f9dac0c036e13d3782ad3e7ac734c7de43496c0a
DerieuxTheo <theo.derieux@outlook.fr>
2022-05-23 14:37:17 +0200

    First Commit

---
+++ flashme2signal/src/main/java/fr/iut/nantes/flashme2signal/Flashme2signalApplication.java
@@ -0,0 +1,13 @@
+package fr.iut.nantes.flashme2signal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Flashme2signalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Flashme2signalApplication.class, args);
+	}
+
+}
---
+++ flashme2signal/src/test/java/fr/iut/nantes/flashme2signal/Flashme2signalApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.iut.nantes.flashme2signal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Flashme2signalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
