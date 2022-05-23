df9ed8e0c6fc74d9fac996f67223ad15c3e2b11c
Juluan Quardon <juluan.quardon29000@gmail.com>
2022-05-23 17:49:26 +0200

    setup LeftMenu

---
+++ web/src/components/LeftMenu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftMenu = () => {
+    return (
+        <div className='LeftMenuContainer'>
+            <div></div>
+        </div>
+    );
+};
+
+export default LeftMenu;
