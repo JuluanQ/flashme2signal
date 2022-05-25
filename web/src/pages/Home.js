56ba160f19d76a9159a189037d9c181f668c11c7
Juluan Quardon <juluan.quardon29000@gmail.com>
2022-05-25 11:08:52 +0200

    Add IssueTable in Home.js

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -6,6 +6,7 @@ import '../assets/css/Home/Home.css'
 // Components
 import StatsCard from '../components/StatsCard';
 import LeftMenu from '../components/LeftMenu';
+import IssueTable from '../components/IssueTable';
 
 const Home = () => {
     return (
@@ -13,6 +14,7 @@ const Home = () => {
             <LeftMenu />
             <div className='Home'>
                 <StatsCard />
+                <IssueTable />
             </div>
         </>
     );
