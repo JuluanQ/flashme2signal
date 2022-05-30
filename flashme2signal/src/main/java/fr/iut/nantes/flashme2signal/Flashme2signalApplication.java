ddad79ad3d2ffaf66255611d830529e10fe3d9b7
DerieuxTheo <theo.derieux@outlook.fr>
2022-05-30 12:17:04 +0200

    (Dev) Modification pour le war et tomcat

--- flashme2signal/src/main/java/fr/iut/nantes/flashme2signal/Flashme2signalApplication.java
+++ flashme2signal/src/main/java/fr/iut/nantes/flashme2signal/Flashme2signalApplication.java
@@ -2,23 +2,19 @@ package fr.iut.nantes.flashme2signal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Flashme2signalApplication {
+public class Flashme2signalApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Flashme2signalApplication.class);
+	}
 
-		/*try {
-			QRCodeGenerator qr = new QRCodeGenerator();
-			qr.generateQR("www.site.fr", "C02_5");
-		} catch (WriterException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}*/
-
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Flashme2signalApplication.class, args);
-
 	}
 
 }
