28bc4a78b3ebf3938b33a544c5704b9628791df1
Juluan Quardon <juluan.quardon29000@gmail.com>
2022-05-30 10:28:57 +0200

    Add DetailDevice and css file fix on witdth in home css

--- web/src/pages/DetailDevice.js
+++ web/src/pages/DetailDevice.js
@@ -1,27 +1,50 @@
+import { Tag } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import ButtonInput from '../components/ButtonInput';
+import IssueTable from '../components/IssueTable';
 import LeftMenu from '../components/LeftMenu';
 
+import '../assets/css/DetailDevice.css'
+
 const DetailDevice = () => {
+
+    const param = useParams()
+
     return (
-        <div>
+        <>
             <LeftMenu />
             <div className='mainContainer'>
                 <div className='nameContainer'>
-
+                    <div className="topNameContainer">
+                        <h3>Appareil n°{param.id}</h3>
+                    </div>
+                    <div className="bottomNameContainer">
+                        <Tag color="green">{0} problèmes</Tag>
+                    </div>
                 </div>
                 <div className='bodyContainer'>
-                    <div>
-
+                    <div className='leftBodyContainer'>
+                        <h4>Salle : </h4>
                     </div>
-                    <div>
-
+                    <div className='rightBodyContainer'>
+                        <div className="qrCodeContainer">
+                            <img src="" alt="" width="10em" height="10em" />
+                        </div>
+                        <div className="buttonsContainer">
+                            <ButtonInput value="Générer" />
+                            <DownloadOutlined className='downloadButton' />
+                        </div>
                     </div>
                 </div>
                 <div className="issuesContainer">
-
+                    <IssueTable />
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
