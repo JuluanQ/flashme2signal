8d3e05d81934b210e0a06e1bcbbdf3d5fe4101a6
JuluanQ <48562224+JuluanQ@users.noreply.github.com>
2022-05-30 22:05:45 +0200

    added fetch on DetailIssue and CSS

--- web/src/pages/DetailIssue.js
+++ web/src/pages/DetailIssue.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import LeftMenu from '../components/LeftMenu';
 import ButtonInput from '../components/ButtonInput';
 
@@ -17,22 +17,124 @@ const { Option } = Select;
 const DetailIssue = () => {
     const params = useParams();
     const navigate = useNavigate();
+
+    const [data, setData] = useState();
+    const [dataIssues, setDataIssues] = useState();
+    const [allIssues, setAllIssues] = useState([]);
+    const [dataDevice, setDataDevice] = useState();
+    const [dateDifference, setDateDifference] = useState(0);
+
+    const severiteColors = (severite) => {
+        severite = severite.toLowerCase();
+        if (severite === "urgent") {
+            return "red"
+        } else if (severite === "moyen") {
+            return "orange"
+        } else if (severite === "mineur") {
+            return "green"
+        }
+    }
+
+    const etatColors = (etat) => {
+        etat = etat.toLowerCase();
+        if (etat === "annulé") {
+            return "red"
+        } else if (etat === "en cours") {
+            return "orange"
+        } else if (etat === "terminé") {
+            return "green"
+        }
+    }
+
+    useEffect(() => {
+
+    }, []);
+
+    useEffect(() => {
+        fetch("http://212.227.3.231:8085/flashme2signal/demande/" + params.id)
+            .then(res => res.json())
+            .then(data => {
+                setData(data);
+            })
+            .catch(err => console.log(err))
+
+    }, []);
+
+    useEffect(() => {
+        if (dataDevice !== undefined && allIssues.length === 0) {
+            fetch("http://212.227.3.231:8085/flashme2signal/demandes/")
+                .then(res => res.json())
+                .then(data => {
+                    data.forEach(issue => {
+                        if (issue.idMateriel !== null && issue.severite === "En cours") {
+                            if (issue.idMateriel.id === dataDevice.id) {
+                                allIssues.push(issue);
+                            }
+                        }
+                    });
+                })
+                .catch(err => console.log(err))
+        }
+    }, [dataDevice]);
+
+    useEffect(() => {
+        if (data !== undefined && dataIssues === undefined) {
+            let json = {
+                "id": data.id,
+                "etat": data.etat,
+                "date": data.dateDemande.split("T")[0],
+                "description": data.description,
+                "severite": data.severite,
+            }
+            if (data.idMateriel !== null) {
+                fetch("http://212.227.3.231:8085/flashme2signal/materiel/" + data.idMateriel.id)
+                    .then(res => res.json())
+                    .then(data => {
+                        let json = {
+                            "id": data.id,
+                            "salle": data.salle,
+                            "type": data.type,
+                        }
+                        setDataDevice(json)
+                    })
+                    .catch(err => console.log(err))
+            }
+            if (data.etat != null) {
+                json.etat = data.etat.libelle;
+            }
+
+            let now = new Date();
+            let date = new Date(data.dateDemande);
+            let diff = dateDiff(date, now);
+            setDateDifference(diff.day);
+
+            setDataIssues(json);
+        }
+    }, [data]);
+
     return (
         <>
             <LeftMenu />
             <div className='ContentIssue'>
                 <p className='IssueTitle'>Demande <span id='IdIssue'>#{params.id}</span></p>
                 <div>
-                    <Tag className="red">Urgent</Tag>
-                    <Tag className="green">En cours</Tag>
+                    {
+                        dataIssues !== undefined ?
+                            <>
+                                <Tag className={severiteColors(dataIssues.severite)}>{dataIssues.severite}</Tag>
+                                <Tag className={etatColors(dataIssues.etat)}>{dataIssues.etat}</Tag>
+                            </>
+                            : <></>
+                    }
+
                 </div>
                 <div className='DetailContentIssue'>
                     <div className='DescriptionIssue'>
                         <p className='DescriptionText'>Description :</p>
-                        <textarea type="textarea" id="iptDesc"></textarea>
-                        <p>Cette demande a été créée il y'a <span id="nCreation">2 jours</span></p>
+                        <p id="iptDesc">{dataIssues !== undefined ? dataIssues.description : "..."}</p>
+                        <p >Cette demande a été créée il y'a <span id="nCreation">{dateDifference} jours</span></p>
                         <p className='DescriptionText'>Sévérité :</p>
-                        <Select className='SelectSeverity' size="small">
+                        <Select className='SelectSeverity' size="small" placeholder={dataIssues ? dataIssues.severite : "..."}>
                             <Option value="Majeur">Urgent</Option>
                             <Option value="Moyen">Moyen</Option>
                             <Option value="Mineur">Mineur</Option>
@@ -43,15 +145,33 @@ const DetailIssue = () => {
                         </div>
                     </div>
                     <div className='ComputerIssue'>
-                        <p className='ComputerTitle'>Ordinateur <span id='IdIssue'>#{params.device}</span></p>
+                        <p className='ComputerTitle'>Appareil <span id='IdIssue'>#{dataDevice ? dataDevice.id : "?"}</span></p>
                         <div className="Contain">
                             <div className="TxtInput">
-                                <InputText title="Salle" type="text" disabled="true" />
-                                <InputText title="Autres problèmes" type="text" disabled="true" />
+                                <div className="deviceInfos">
+                                    <p>Salle :</p>
+                                    {dataDevice !== undefined ? <p className="deviceInfo">{dataDevice.salle}</p> : <p className="deviceInfo">...</p>}
+                                </div>
+                                <div className="deviceInfos">
+                                    <p>Type :</p>
+                                    {dataDevice !== undefined ? <p className="deviceInfo">{dataDevice.type}</p> : <p className="deviceInfo">...</p>}
+                                </div>
+                                <div className='deviceInfos'>
+                                    <p>Autres Problemes :</p>
+                                    {allIssues.length > 0 ?
+                                        <div className="deviceInfo">
+                                            {allIssues.map(issue => (<Tag className={severiteColors(issue.severite)}>{issue.id}</Tag>))}
+                                        </div>
+                                        : <p className="deviceInfo">...</p>}
+                                </div>
                             </div>
-                            <div className='boutons-descripfion' onClick={() => navigate('/DetailDevice')}>
-                                <ButtonInput value="Voir plus" className="BtnVoirPlus" ></ButtonInput>
-                            </div>
+                            {
+                                dataDevice !== undefined ?
+                                    <div className='boutons-descripfion' onClick={() => navigate('/DetailDevice/' + dataDevice.id)}>
+                                        <ButtonInput value="Voir plus" className="BtnVoirPlus" ></ButtonInput>
+                                    </div> : <></>
+                            }
+
                         </div>
                     </div>
                 </div>
@@ -61,3 +181,22 @@ const DetailIssue = () => {
 };
 
 export default DetailIssue;
+
+function dateDiff(date1, date2) {
+    var diff = {}                           // Initialisation du retour
+    var tmp = date2 - date1;
+
+    tmp = Math.floor(tmp / 1000);             // Nombre de secondes entre les 2 dates
+    diff.sec = tmp % 60;                    // Extraction du nombre de secondes
+
+    tmp = Math.floor((tmp - diff.sec) / 60);    // Nombre de minutes (partie entière)
+    diff.min = tmp % 60;                    // Extraction du nombre de minutes
+
+    tmp = Math.floor((tmp - diff.min) / 60);    // Nombre d'heures (entières)
+    diff.hour = tmp % 24;                   // Extraction du nombre d'heures
+
+    tmp = Math.floor((tmp - diff.hour) / 24);   // Nombre de jours restants
+    diff.day = tmp;
+
+    return diff;
+}
