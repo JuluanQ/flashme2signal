2f8e70f699a55deda68eaed5b4ca2f1d9bff9ac7
JuluanQ <48562224+JuluanQ@users.noreply.github.com>
2022-05-30 20:43:06 +0200

    added a reset button w/ CSS

--- web/src/components/StatsCard.js
+++ web/src/components/StatsCard.js
@@ -5,6 +5,7 @@ import '../assets/css/Home/StatsCard.css'
 // Icons
 import { GoFileDirectory, GoIssueOpened } from 'react-icons/go';
 import { RiAlarmWarningFill } from 'react-icons/ri';
+import ButtonInput from './ButtonInput';
 
 
 const StatsCard = (props) => {
@@ -12,13 +13,13 @@ const StatsCard = (props) => {
     const DemandeFilters = {
         open: "EN COURS",
         urgent: "URGENT",
-        new: new Date().toISOString().split('T')[0]
+        new: new Date().toISOString().split('T')[0],
+        none: ""
     };
 
     function filterData(filter) {
         var trs = document.querySelectorAll('tbody tr');
-        for (var i = 1; i < trs.length; i++)
-        {
+        for (var i = 1; i < trs.length; i++) {
             var td = null
             if (filter === DemandeFilters.open) {
                 td = trs[i].getElementsByTagName("td")[7];
@@ -26,56 +27,64 @@ const StatsCard = (props) => {
                 td = trs[i].getElementsByTagName("td")[6];
             } else if (filter === DemandeFilters.new) {
                 td = trs[i].getElementsByTagName("td")[2];
-                console.log("test")
+            }
+            else if (filter === DemandeFilters.none) {
+                trs[i].style.display = "";
+                document.body.getElementsByClassName('buttonReset')[0].style.display = "none";
             }
 
-            if (td)
-            {
+            if (td && filter !== DemandeFilters.none) {
                 var txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1)
-                {
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
                     trs[i].style.display = "";
                 } else {
                     trs[i].style.display = "none";
                 }
+                document.body.getElementsByClassName('buttonReset')[0].style.display = "";
             }
         }
     }
 
     return (
         <>
-            <div className='StatsComponents'>
 
-                <div className='CardOpenIssue hvr-grow' onClick={() => filterData(DemandeFilters.open)}>
-                    <div className='CardSVG'>
-                        <GoFileDirectory className='imgStatsComp' />
+            <div className='StatsCardMainContainer'>
+                <div className='StatsComponents'>
+
+                    <div className='CardOpenIssue hvr-grow' onClick={() => filterData(DemandeFilters.open)}>
+                        <div className='CardSVG'>
+                            <GoFileDirectory className='imgStatsComp' />
+                        </div>
+                        <div className='CardText'>
+                            <p className='cardName'>Dossiers ouverts</p>
+                            <p className='nCardOpenIssue cardValue'>{props.open}</p>
+                        </div>
                     </div>
-                    <div className='CardText'>
-                        <p className='cardName'>Dossiers ouverts</p>
-                        <p className='nCardOpenIssue cardValue'>{props.open}</p>
+
+                    <div className='CardOpenIssue hvr-grow' onClick={() => filterData(DemandeFilters.new)}>
+                        <div className='CardSVG'>
+                            <GoIssueOpened className='imgStatsComp' />
+                        </div>
+                        <div className='CardText'>
+                            <p className='cardName'>Nouveaux dossiers</p>
+                            <p className='nCardNewIssue cardValue'>{props.new}</p>
+                        </div>
                     </div>
+
+                    <div className='CardImportantIssue hvr-grow' onClick={() => filterData(DemandeFilters.urgent)}>
+                        <div className='CardSVG'>
+                            <RiAlarmWarningFill className='imgImportantComp' />
+                        </div>
+                        <div className='CardText'>
+                            <p className='cardName'>Dossiers urgents</p>
+                            <p className='nCardImportantIssue cardValue'>{props.urgent}</p>
+                        </div>
+                    </div>
+
                 </div>
-
-                <div className='CardOpenIssue hvr-grow' onClick={() => filterData(DemandeFilters.new)}>
-                    <div className='CardSVG'>
-                        <GoIssueOpened className='imgStatsComp' />
-                    </div>
-                    <div className='CardText'>
-                        <p className='cardName'>Nouveaux dossiers</p>
-                        <p className='nCardNewIssue cardValue'>{props.new}</p>
-                    </div>
+                <div className="buttonReset" onClick={() => filterData(DemandeFilters.none)} style={{ display: "none" }}>
+                    <ButtonInput value="Reset" />
                 </div>
-
-                <div className='CardImportantIssue hvr-grow' onClick={() => filterData(DemandeFilters.urgent)}>
-                    <div className='CardSVG'>
-                        <RiAlarmWarningFill className='imgImportantComp' />
-                    </div>
-                    <div className='CardText'>
-                        <p className='cardName'>Dossiers urgents</p>
-                        <p className='nCardImportantIssue cardValue'>{props.urgent}</p>
-                    </div>
-                </div>
-
             </div>
         </>
     );
