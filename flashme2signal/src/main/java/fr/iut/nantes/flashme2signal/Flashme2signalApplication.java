b41dcc1d66a4c9556b261a07b46d20c504a3a654
DerieuxTheo <theo.derieux@outlook.fr>
2022-05-30 11:01:43 +0200

    (Dev) Fix it

--- flashme2signal/src/main/java/fr/iut/nantes/flashme2signal/Flashme2signalApplication.java
+++ flashme2signal/src/main/java/fr/iut/nantes/flashme2signal/Flashme2signalApplication.java
@@ -8,14 +8,14 @@ public class Flashme2signalApplication {
 
 	public static void main(String[] args) {
 
-		try {
+		/*try {
 			QRCodeGenerator qr = new QRCodeGenerator();
 			qr.generateQR("www.site.fr", "C02_5");
 		} catch (WriterException e) {
 			throw new RuntimeException(e);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
-		}
+		}*/
 
 		SpringApplication.run(Flashme2signalApplication.class, args);
 
