6e92de99377f54918820488c8cc40d4b7d0782e4
Nolwenn CLOAREC <nolwenn.cloarec@etu.univ-nantes.fr>
2022-06-02 10:57:04 +0200

    Modifs couleurs btn (terminer, en cours, annuler) au hover Modifs css page appareils

--- web/src/pages/DetailIssue.js
+++ web/src/pages/DetailIssue.js
@@ -189,7 +189,7 @@ const DetailIssue = () => {
                             color: '#fff',
                             border: 'none',
                             borderRadius: '4px',
-                            textAlign: 'left',
+                            
                             padding: '10px',
                         },
                         placement: "topRight",
@@ -275,30 +275,30 @@ const DetailIssue = () => {
         if (dataIssues !== undefined) {
             if (dataIssues.etat === "En cours") {
                 setHtmlButtons(<>
-                    <div onClick={() => handleTerminer(data)}>
-                        <ButtonInput value="Terminer" color="green" />
+                    <div class="btn-green" onClick={() => handleTerminer(data)}>
+                        <ButtonInput value="Terminer" color="green"  />
                     </div>
-                    <div onClick={() => handleAnnuler(data)}>
-                        <ButtonInput value="Annuler" color="red" />
+                    <div class="btn-red" onClick={() => handleAnnuler(data)}>
+                        <ButtonInput value="Annuler" color="red"  />
                     </div>
                 </>)
             }
             if (dataIssues.etat === "Terminé") {
                 setHtmlButtons(<>
-                    <div onClick={() => handleEnCours(data)}>
-                        <ButtonInput value="En cours" color="orange" />
+                    <div  class="btn-orange" onClick={() => handleEnCours(data)}>
+                        <ButtonInput value="En cours" color="orange"/>
                     </div>
-                    <div onClick={() => handleAnnuler(data)}>
+                    <div class="btn-red" onClick={() => handleAnnuler(data)}>
                         <ButtonInput value="Annuler" color="red" />
                     </div>
                 </>)
             }
             if (dataIssues.etat === "Annulé") {
                 setHtmlButtons(<>
-                    <div onClick={() => handleTerminer(data)}>
+                    <div class="btn-green" onClick={() => handleTerminer(data)}>
                         <ButtonInput value="Terminer" color="green" />
                     </div>
-                    <div onClick={() => handleEnCours(data)}>
+                    <div class="btn-orange" onClick={() => handleEnCours(data)}>
                         <ButtonInput value="En cours" color="orange" />
                     </div>
                 </>)
